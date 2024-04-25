3fea33bb54ba525f3cd5ec37463d617e46cf957f
priyanka2121999 <emailID>
2024-04-25 12:26:21 +0000

    Initial Project

---
+++ src/test/java/testScripts/GooglePageTest.java
@@ -0,0 +1,15 @@
+package testScripts;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class GooglePageTest {
+	
+	public static void main(String[] args) {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.google.com/");
+	
+	}
+
+}
